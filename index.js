51cbb3106fe052c6f8d44ef2a6579793d8f7d495
arbdt <67365149+arbdt@users.noreply.github.com>
2020-10-17 16:32:03 +1030

    adding base files to repo

---
+++ index.js
@@ -0,0 +1,29 @@
+// import packages
+let fs = require("fs");
+let inquirer = require("inquirer");
+let utils = require("utils");
+
+// array of questions for user
+const questions = [
+    {name: "titleEntry"},
+    {name: "descEntry"},
+    {name: "installEntry"},
+    {name: "usageEntry"},
+    {name: "contribEntry"},
+    {name: "testsEntry"},
+    {name: "licenceEntry"},
+    {name: "githubEntry"},
+    {name: "emailEntry"}
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();

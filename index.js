af6091808ae5e1d38434abe852169896dd85c369
arbdt <67365149+arbdt@users.noreply.github.com>
2020-10-17 17:09:46 +1030

    added list of questions

--- index.js
+++ index.js
@@ -5,15 +5,52 @@ let utils = require("utils");
 
 // array of questions for user
 const questions = [
-    {name: "titleEntry"},
-    {name: "descEntry"},
-    {name: "installEntry"},
-    {name: "usageEntry"},
-    {name: "contribEntry"},
-    {name: "testsEntry"},
-    {name: "licenceEntry"},
-    {name: "githubEntry"},
-    {name: "emailEntry"}
+    {
+        type: "input",
+        name: "titleEntry",
+        message: "Enter a title for the Readme:"
+    },
+    {
+        type: "input",
+        name: "descEntry",
+        message: "Enter a decription for this project:"
+    },
+    {
+        type: "input",
+        name: "installEntry",
+        message: "Enter installation instructions:"
+    },
+    {
+        type: "input",
+        name: "usageEntry",
+        message: "Enter usage instructions:"
+    },
+    {
+        type: "input",
+        name: "contribEntry",
+        message: "Enter contribution guidelines:"
+    },
+    {
+        type: "input",
+        name: "testEntry",
+        message: "Enter test instructions:"
+    },
+    {
+        type: "list",
+        name: "licenceEntry",
+        message: "Choose a licence:",
+        choices: ["MIT", "GNU", "Apache"]
+    },
+    {
+        type: "input",
+        name: "githubEntry",
+        message: "Enter your GitHub username:"
+    },
+    {
+        type: "input",
+        name: "emailEntry",
+        message: "Enter your email address:"
+    }
 ];
 
 // function to write README file

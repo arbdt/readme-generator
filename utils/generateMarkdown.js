710d982efa0883607f4f53720c74a32e8892b267
arbdt <67365149+arbdt@users.noreply.github.com>
2020-10-18 14:06:56 +1030

    fleshed out readme template with placeholder variables

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,34 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}\n
+
+  ## Table of Contents
+  - Installation
+  - Usage
+  - Licence
+  - Contributing
+  - Tests
+  - Questions\n
+
+  ## Installation
+  ${data.installation}\n
+
+  ## Usage
+  ${data.usage}\n
+
+  ## License
+  This project is licenced under the ${data.licence} licence.\n
+
+  ## Contributing
+  ${data.contribution}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions or comments, I may be contacted at [GitHub](https://github.com/${data.github}), or by [email](mailto:${data.email}).
 `;
 }
 
